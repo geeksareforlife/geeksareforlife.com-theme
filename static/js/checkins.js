b3e0aaf2ab8ddaba574f45d13d6da30d2ec52e87
James Hayward <jhayward1980@gmail.com>
2020-08-19 18:45:52 +0100

    Theming the map pins

--- static/js/checkins.js
+++ static/js/checkins.js
@@ -14,7 +14,18 @@ function showSinglePoint() {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mymap);
 
-  var marker = L.marker([lat, long]).addTo(mymap);
+  var gr4lIcon = L.icon({
+    iconUrl: '/images/map-pin.png',
+    shadowUrl: '/images/map-pin-shadow.png',
+
+    iconSize:     [25, 45], // size of the icon
+    shadowSize:   [46, 37], // size of the shadow
+    iconAnchor:   [12, 45], // point of the icon which will correspond to marker's location
+    shadowAnchor: [12, 33],  // the same for the shadow
+    popupAnchor:  [1, -35] // point from which the popup should open relative to the iconAnchor
+  });
+
+  var marker = L.marker([lat, long], {icon: gr4lIcon}).addTo(mymap);
   marker.bindPopup(popup).openPopup();
 
 }
@@ -23,7 +34,7 @@ function showManyPoints() {
   var mymap = L.map('fullMap').setView([52.921711, -1.472167], 10);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 14,
+    maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mymap);
 
@@ -35,6 +46,10 @@ function showManyPoints() {
       popup += 's'; 
     }
     popup += "</a><br>Last Visited: " + trigs[point]["last"] + "</p>";
-    L.circleMarker([trigs[point]["lat"], trigs[point]["long"]], {radius: 2}).addTo(mymap).bindPopup(popup);
+    L.circleMarker([trigs[point]["lat"], trigs[point]["long"]], 
+      {
+        radius: 10,
+        color: "#9b2c2c"
+      }).addTo(mymap).bindPopup(popup);
   }
 }
